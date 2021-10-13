51df2b6bb6648642369d70564daf3a076d6dff3b
Youngminah <bluebelsmudi@gmail.com>
2021-10-13 09:40:01 +0900

    carthage

---
+++ YMLogoAlert/YMLogoAlert.h
@@ -0,0 +1,18 @@
+//
+//  YMLogoAlert.h
+//  YMLogoAlert
+//
+//  Created by meng on 2021/10/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YMLogoAlert.
+FOUNDATION_EXPORT double YMLogoAlertVersionNumber;
+
+//! Project version string for YMLogoAlert.
+FOUNDATION_EXPORT const unsigned char YMLogoAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMLogoAlert/PublicHeader.h>
+
+
